dfeac8a7941cb089f46fb353f4d25e09fdb60d0e
Mr. Robot | Android App Developer <88169054+devxvoid@users.noreply.github.com>
2026-08-30 12:57:13 +0400

    feat: register torrent MethodChannel in Flutter engine

--- android/app/src/main/kotlin/com/scenebox/MainActivity.kt
+++ android/app/src/main/kotlin/com/scenebox/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.scenebox
 
 import android.content.Intent
-import android.net.Uri
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
     private val channelName = "scenebox/android"
+    private val torrentChannelName = "scenebox/torrent"
     private var channel: MethodChannel? = null
+    private var torrentChannel: MethodChannel? = null
     private var player: PlayerBridge? = null
+    private var torrent: TorrentMethodChannel? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
         channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName)
         player = PlayerBridge(this)
         channel?.setMethodCallHandler(PlaybackChannel(this, player!!))
+
+        torrentChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, torrentChannelName)
+        torrent = TorrentMethodChannel(this)
+        torrent?.attach(torrentChannel!!)
 
         intent?.data?.toString()?.let { link ->
             channel?.invokeMethod("initialDeepLink", link)
@@ -25,10 +32,16 @@ class MainActivity : FlutterActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
-        intent.data?.toString()?.let { channel?.invokeMethod("onDeepLink", it) }
+        intent.data?.toString()?.let {
+            channel?.invokeMethod("onDeepLink", it)
+        }
     }
 
     override fun onDestroy() {
+        torrent?.close()
+        torrent = null
+        torrentChannel?.setMethodCallHandler(null)
+        torrentChannel = null
         player?.release()
         player = null
         channel?.setMethodCallHandler(null)
